99d29b4fd34c5cd6b7dd6ab4f5cea2cbadf98d41
Hiroshi Miura <miurahr@linux.com>
2019-08-13 06:38:08 +0900

    Pack into jar joml 1.9.16
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,24 @@
-import org.openstreetmap.josm.gradle.plugin.config.I18nConfig
-import org.openstreetmap.josm.gradle.plugin.config.JosmManifest
-
 plugins {
-  id("org.openstreetmap.josm") version "0.6.1"
-  java
-}
-
-repositories {
-  jcenter()
+    id("org.openstreetmap.josm") version "0.6.1"
+    java
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 base.archivesBaseName = "joml"
+project.version = "1.9.16"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    packIntoJar("org.joml:joml:1.9.16")
+}
 
 tasks {
-  "processResources"(ProcessResources::class) {
-    from(project.projectDir) {
-      include("LICENSE")
+    "processResources"(ProcessResources::class) {
+        from(project.projectDir) {
+            include("LICENSE")
+        }
     }
-  }
 }
